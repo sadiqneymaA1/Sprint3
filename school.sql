6e6c51820fca1c64fa346e6ddc8bfa2e5c09be6d
sadiqneymaA1 <97371508+sadiqneymaA1@users.noreply.github.com>
2022-03-20 22:54:12 -0600

    Delete school.sql

--- school.sql
+++
@@ -1,5 +0,0 @@
-insert into department (id_department,name, code) VALUES
-(DEFAULT,"Computer information technology","CIT"),
-(DEFAULT,'Economics', 'ECON'),
-(default,'Humanities and Philosophy','HUM')
-
