514a60bc420508890d686a5ccabf60add151ecee
sadiqneymaA1 <97371508+sadiqneymaA1@users.noreply.github.com>
2022-03-20 22:46:35 -0600

    Delete exercise 2a.sql

--- exercise 2a.sql
+++
@@ -1,4 +0,0 @@
-SELECT vendor_name,i.Invoice_number,i.Invoice_date,(invoice_total - payment_total - credit_total) AS balance_due
- FROM Vendors AS v 
- INNER JOIN Invoices AS i ON v.vendor_id = i.vendor_id
- WHERE (invoice_total - payment_total - credit_total) > 0
