417af7bee515fb0ccd82fb7b23f160f2c5f45d8c
sadiqneymaA1 <97371508+sadiqneymaA1@users.noreply.github.com>
2022-03-20 22:53:35 -0600

    Delete import.py

--- import.py
+++
@@ -1,7 +0,0 @@
-import math
-wtire=float(input("Enter the width of the tire in cm (ex 205): "))
-atire=float(input("Enter the aspect ratio of the tire (ex 60): "))
-dmeater=float(input("Enter the diameter of the tire in inches (ex 15): "))
-
-v=math.pi*wtire*wtire*atire*((wtire*atire)+(2540*dmeater))/10000000
-print(round(v,1))
