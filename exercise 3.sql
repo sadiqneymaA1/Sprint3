ce2ecf07deb0c477dcd49566d715658d3fd6bad3
sadiqneymaA1 <97371508+sadiqneymaA1@users.noreply.github.com>
2022-03-20 22:48:46 -0600

    Delete exercise 3.sql

--- exercise 3.sql
+++
@@ -1,5 +0,0 @@
-SELECT g.account_number, account_description, invoice_id
-FROM general_ledger_accounts AS g 
-Left OUTER JOIN invoice_line_items AS i ON  g.account_number = i.account_number
-WHERE invoice_id is NULL
-order by g.account_number;
