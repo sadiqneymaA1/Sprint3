7ebd4a67bd9ee0291dee801f069c4e694d6d2dc7
sadiqneymaA1 <97371508+sadiqneymaA1@users.noreply.github.com>
2022-03-20 22:44:41 -0600

    Delete exercise 1.sql

--- exercise 1.sql
+++
@@ -1,8 +0,0 @@
-use ap;
-SELECT v.vendor_name,invoice_date,invoice_number, li.invoice_sequence AS li_amount
-FROM Vendors v
-JOIN Invoices i
-ON v.vendor_id=i.vendor_id
-JOIN invoice_line_items li
-ON i.invoice_id=li.invoice_id AND i.invoice_id = 12
-ORDER BY v.vendor_name,i.invoice_date,invoice_number,li.invoice_sequence;
