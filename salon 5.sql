d99f8e9b97a3b1004449f197e6b7f4bc324adcc5
sadiqneymaA1 <97371508+sadiqneymaA1@users.noreply.github.com>
2022-03-20 22:50:39 -0600

    Delete salon 5.sql

--- salon 5.sql
+++
@@ -1,9 +0,0 @@
-insert into city(id_city,city_name,state)
-values(default,'Dallas','TX'),
-(default,'Provo','UT'),
-(default,'Mesa','AZ'),
-(default,'Topeka','KS'),
-(default,'Rigby','ID'),
-(default,'Bozeman','MT'),
-(default,'Decatur','TN'),
-(default,'Rexburg','ID')
